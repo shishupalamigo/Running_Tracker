97e306cf9e9104d996d25e9960b7afb298d6c388
shishupalamigo <shishupalkr2595@gmail.com>
2020-09-15 23:22:03 +0530

    Add caculations for total and average

--- script.js
+++ script.js
@@ -5,11 +5,27 @@ const enteriesWrapper = document.querySelector("#entries");
 function addNewEntry(newEntry) {
     enteriesWrapper.removeChild(enteriesWrapper.firstElementChild);
    const listItem = document.createElement('li');
-   const listValue = document.createTextNode(newEntry);
+   const listValue = document.createTextNode(newEntry.toFixed(1));
    listItem.appendChild(listValue);
-
    enteriesWrapper.appendChild(listItem);
 }
+
+function reducer(total, currentValue) {
+    return total + currentValue;
+}
+
+function calcTotal() {
+    const totalValue = entries.reduce(reducer).toFixed(1);
+    document.getElementById("total").innerText = totalValue;
+    document.getElementById("progressTotal").innerText = totalValue;
+
+}
+
+function calcAverage() {
+    const average = (entries.reduce(reducer) / entries.length).toFixed(1);
+    document.getElementById("average").innerText = average;
+}
+
 function handleSubmit(event) {
     event.preventDefault()
     const entry = Number(document.querySelector('#entry').value);
@@ -17,6 +33,8 @@ function handleSubmit(event) {
     document.querySelector("form").reset();
     entries.push(entry);
     addNewEntry(entry);
+    calcTotal();
+    calcAverage();
 }
 
 const form = document
